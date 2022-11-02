efa1134d8671ceda0e6328cdb80019399ab48043
jayeshfw230025 <jayeshmasavi2022@gmail.com>
2022-11-02 16:19:17 +0530

    code fixed for primenumber

--- primenumber.js
+++ primenumber.js
@@ -2,13 +2,13 @@ prime problem
 let num=13
 check the given number is prime or not
   let count=0;
-    for (i=1;i<=num;i+){
-        if(num%i!==0)
+    for (i=1;i<=num;i++){
+        if(num%i==0)
         count++;
     
     }
-    if(coun==2){
-console.log("yes ");
+    if(count==2){
+console.log("Yes");
 }else {
 console.log("No");
 
