6ea1d1658f017a0fd89814209cc9ad2cabe3fa78
jayeshfw230025 <jayeshmasavi2022@gmail.com>
2022-11-02 12:18:31 +0530

    Prime number

---
+++ primenumber.js
@@ -0,0 +1,15 @@
+prime problem
+let num=13
+check the given number is prime or not
+  let count=0;
+    for (i=1;i<=num;i+){
+        if(num%i!==0)
+        count++;
+    
+    }
+    if(coun==2){
+console.log("yes ");
+}else {
+console.log("No");
+
+}
